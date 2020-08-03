61964d2f0a814bc2c997a2a0bb32a3b9b1a65ccc
Zaz <zazorah@gmail.com>
2020-08-03 09:26:30 -0500

    Okay I did some more stuff too
    -Frrrrt hhaha :)

--- flaskvers/main.py
+++ flaskvers/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import twitter
+import tweepy
 
 app = Flask(__name__)
 
@@ -6,6 +8,14 @@ app = Flask(__name__)
 def MainScreen():
 
     return render_template('index.html')
+
+@app.route('/<int:id>', methods = ['POST'])
+def DoRetweets(id):
+
+    #Put Python Code Here
+
+    winners = ['name1', 'name2', 'name3']
+    return jsonify({'data': render_template('response.html', list = winners)})
 
 if __name__ == "__main__":
 
